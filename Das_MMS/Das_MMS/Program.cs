19333fc95085f4225c2a3385adef09439cf28172
SW-SG-SCHRAVEN-M <M.Schraven@stadtwerke-solingen.de>
2023-02-23 08:55:23 +0100

    commit

--- Das_MMS/Das_MMS/Program.cs
+++ Das_MMS/Das_MMS/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.EntityFramework;
+using Spectre.Console;
 
 namespace Das_MMS
 {
@@ -10,7 +12,17 @@ namespace Das_MMS
     {
         static void Main(string[] args)
         {
+            System.Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WindowHeight = 42;
+            Console.WindowWidth = 150;
+            Console.Title = "Das_MMS";
 
+            var image = new CanvasImage("MMslogo.png");
+            image.MaxWidth(64);
+            image.BilinearResampler();
+            AnsiConsole.Write(image);
+
+            Console.ReadLine();
         }
     }
 }
